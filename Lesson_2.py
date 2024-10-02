5076a8d6f70121d3988dca6ef9d1f5e7f32b86c4
Pavel-1572 <btcsapsan1972@gmail.com>
2024-10-02 15:34:20 +0800

    Add files via upload

---
+++ Lesson_2.py
@@ -0,0 +1,17 @@
+first = 123
+second = 456
+third = 789
+if first == second and first == third:
+    print(3)
+elif first != second and first != third and second != third:
+    print(0)
+elif first == second or first == third or second == third:
+    print(2)
+
+
+
+
+
+
+
+
